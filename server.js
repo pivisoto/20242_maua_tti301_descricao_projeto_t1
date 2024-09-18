cce0a6e198861205b471e605636e1c329ebccb4f
pivisoto <pivisoto10@gmail.com>
2024-09-17 21:23:18 -0300

    feat(t1): obtem previsao a partir de coords

--- server.js
+++ server.js
@@ -7,13 +7,12 @@ const app = express();
 const API_KEY = process.env.OPENWEATHER_API_KEY;
 const server = http.createServer(app);
 
-// Configuração do readline para capturar a entrada do usuário
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-// Função para obter as coordenadas da cidade
 async function getCityCoordinates(city) {
     try {
         const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
@@ -26,19 +25,30 @@ async function getCityCoordinates(city) {
         } else {
             const { lat, lon } = data[0];
             console.log(`Segue as informações de ${city} - Latitude: ${lat}, Longitude: ${lon}`);
+            await getWeatherData(lat, lon);
         }
     } catch (error) {
         console.error('Erro ao chamar a API:', error.response ? error.response.data : error.message);
     }
 }
 
-// Pergunta para o usuário digitar a cidade
+async function getWeatherData(lat, lon) {
+    try {
+        const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+        const response = await axios.get(url);
+        const data = response.data;
+
+        const feelsLike = data.main.feels_like;
+        const description = data.weather[0].description;
+
+        console.log(`Sensação térmica: ${feelsLike}°C, Descrição: ${description}`);
+    } catch (error) {
+        console.error('Erro ao obter o clima:', error.response ? error.response.data : error.message);
+    }
+}
+
 rl.question('Digite o nome da cidade: ', (city) => {
     getCityCoordinates(city);
-    rl.close(); // Fecha a interface readline após a resposta
+    rl.close(); 
 });
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log(`Servidor rodando na porta ${PORT}`);
-// });
